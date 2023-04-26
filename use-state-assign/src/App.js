27f348254b8b6be32a1061f7a00aec4c02392f86
shivu <shivanithoke3108@gmail.com>
2023-04-26 23:20:23 +0530

    usestate

---
+++ use-state-assign/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterApp from './Component/counter';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
