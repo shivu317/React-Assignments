65564bc7a7c2dad779e520bcce96f5f92d7b45a7
shivu <shivanithoke3108@gmail.com>
2023-05-12 06:24:35 +0530

    I Have Created Game App

---
+++ tic-tac-toe/src/Components/TicTacToe.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './TicTacToe.css';
+
+const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const handleClick = (i) => {
+    const squares = [...board];
+    if (winner || squares[i]) {
+      return;
+    }
+    squares[i] = player;
+    setBoard(squares);
+    setPlayer(player === 'X' ? 'O' : 'X');
+    setWinner(checkWinner(squares));
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setPlayer('X');
+    setWinner(null);
+  };
+
+  const renderSquare = (i) => {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {board[i]}
+      </button>
+    );
+  };
+
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${player}`;
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board">
+        <div className="row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <button className="reset" onClick={resetGame}>Reset Game</button>
+    </div>
+  );
+};
+
+export default TicTacToe;
