ed41b08c97efbd56346fa8b70fac4ae6eafc8820
shivu <shivanithoke3108@gmail.com>
2023-06-30 14:41:25 +0530

    done

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './Calculator.css';
+
+
+const Calculator = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleAddition = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+  };
+
+  const handleSubtraction = () => {
+    const difference = parseFloat(num1) - parseFloat(num2);
+    setResult(difference);
+  };
+
+  const handleMultiplication = () => {
+    const product = parseFloat(num1) * parseFloat(num2);
+    setResult(product);
+  };
+
+  const handleDivision = () => {
+    const quotient = parseFloat(num1) / parseFloat(num2);
+    setResult(quotient);
+  };
+
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <input
+        type="number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <input
+        type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <button onClick={handleAddition}>Addition</button>
+      <button onClick={handleSubtraction}>Subtraction</button>
+      <button onClick={handleMultiplication}>Multiplication</button>
+      <button onClick={handleDivision}>Division</button>
+      <h2>Result: {result}</h2>
+    </div>
+  );
+};
+
+export default Calculator;
